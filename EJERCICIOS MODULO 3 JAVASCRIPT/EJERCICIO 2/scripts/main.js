6b552c1be6ff6197cce6e7be0329aba111631b9e
puyhg5 <puyab500@gmail.com>
2017-04-22 21:56:22 +0200

    Two ways to do
    There are two ways to do the exercise

--- EJERCICIOS MODULO 3 JAVASCRIPT/EJERCICIO 2/scripts/main.js
+++ EJERCICIOS MODULO 3 JAVASCRIPT/EJERCICIO 2/scripts/main.js
@@ -1,28 +1,25 @@
 var suits = ["Corazones", "Diamantes", "Treboles", "Picas"];
+var cardsBySuit = 12;
 var cards = [];
-var j = 0;
-for (i=0; i<4; i=i+1){
-  for (a=1; a<=12; a=a+1){
-    cards[j] = {valor: a, palo: suits[i]};
-    j=j+1;
+for (var i=0; i<suits.length; i=i+1){
+  for (var a=1; a<=cardsBySuit; a=a+1){
+    cards[cards.length] = {valor: a, palo: suits[i]};
   }
 }
 
 console.log("Las cartas rojas son:"); //Para comprobar que funciona bien aunque no lo pida el ejercicio
-var a = 0;
 var rojas = [];
-for (j=0; j<=47; j=j+1){
+for (j=0; j<cards.length; j=j+1){
   if (cards[j].palo === "Corazones" || cards[j].palo === "Diamantes"){
-    rojas[a] = cards[j];
-    console.log(rojas[a].valor + " " + rojas[a].palo);
-    a = a + 1;
+    rojas[rojas.length] = cards[j];
+    console.log(rojas[(rojas.length)-1].valor + " " + rojas[(rojas.length)-1].palo);
   }
 }
 
 console.log("Las cartas negras son:") //Para comprobar que funciona bien aunque no lo pida el ejercicio
 var b = 0;
 var negras = [];
-for (j=0; j<=47; j=j+1){
+for (j=0; j<cards.length; j=j+1){
   if (cards[j].palo === "Treboles" || cards[j].palo === "Picas"){
     negras[b] = cards[j];
     console.log(negras[b].valor + " " + negras[b].palo);
