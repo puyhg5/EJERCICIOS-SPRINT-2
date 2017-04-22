20dff597e73d37bb3f3fb1ce8e00476735172bd6
puyhg5 <puyab500@gmail.com>
2017-04-22 21:33:15 +0200

    Use cards.length
    Because with j=j+1 create a one more value in the array.

--- EJERCICIOS MODULO 3 JAVASCRIPT/EJERCICIO 1/scripts/main.js
+++ EJERCICIOS MODULO 3 JAVASCRIPT/EJERCICIO 1/scripts/main.js
@@ -1,16 +1,14 @@
 var suits = ["Corazones", "Diamantes", "Treboles", "Picas"];
 var cardsBySuit = 12;
 var cards = [];
-var j = 0;
-for (i=0; i<suits.length; i=i+1){
+for (var i=0; i<suits.length; i=i+1){
   for (a=1; a<=cardsBySuit; a=a+1){
-    cards[j] = {valor: a, palo: suits[i]}; //Otra opción cards[cards.length]
-    j=j+1;
+    cards[cards.length] = {valor: a, palo: suits[i]};
   }
 }
 
-console.log("En esta baraja hay " + j + " cartas. Son las siguientes:"); //Para comprobar que funciona bien aunque no lo pida el ejercicio
+console.log("En esta baraja hay " + cards.length + " cartas. Son las siguientes:"); //Para comprobar que funciona bien aunque no lo pida el ejercicio
 
-for (j=0; j<=47; j=j+1){
+for (j=0; j<cards.length; j=j+1){
   console.log(cards[j].valor + " " + cards[j].palo);
 }
