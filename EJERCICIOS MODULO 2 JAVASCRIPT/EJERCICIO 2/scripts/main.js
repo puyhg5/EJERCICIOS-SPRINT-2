ad1fb5c00adb814e5a162adc812dacc21ccb26bf
puyhg5 <puyab500@gmail.com>
2017-04-22 20:15:26 +0200

    Replace i<=2050 by i<2050
    Replace because the statement say before 2050.

--- EJERCICIOS MODULO 2 JAVASCRIPT/EJERCICIO 2/scripts/main.js
+++ EJERCICIOS MODULO 2 JAVASCRIPT/EJERCICIO 2/scripts/main.js
@@ -2,6 +2,6 @@ var nextLeapYear = 2020;
 var finalYear = 2050;
 var leapFactor = 4;
 
-for (var i=nextLeapYear; i<=finalYear; i=i+leapFactor){
+for (var i=nextLeapYear; i<finalYear; i=i+leapFactor){
   console.log (i + " es un año bisiesto");
 }
