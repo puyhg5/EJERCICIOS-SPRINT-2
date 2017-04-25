d5941fffba2f61c4407caa732104bb6efe4272a3
puyhg5 <puyab500@gmail.com>
2017-04-25 23:56:25 +0200

    EXERCISE 1 OF MODULE 5
    Upload exercise one of module five.

---
+++ EJERCICIOS MODULO 5 JAVASCRIPT/EJERCICIO 1/scripts/main.js
@@ -0,0 +1,34 @@
+var suits = ["Corazones", "Diamantes", "Treboles", "Picas"];
+var cardsBySuit = 12;
+var cards = [];
+for (var i=0; i<suits.length; i=i+1){
+  for (a=1; a<=cardsBySuit; a=a+1){
+    cards[cards.length] = {valor: a, palo: suits[i]};
+  }
+}
+firstIndexCards = 0;
+lastIndexCards = cards.length - 1;
+
+function barajar(cards){
+  for (var iteration=1; iteration<=100; iteration++){
+    var indexRandom = Math.floor(Math.random() * (lastIndexCards - firstIndexCards) + firstIndexCards);
+    var indexRandom2 = Math.floor(Math.random() * (lastIndexCards - firstIndexCards) + firstIndexCards);
+    while (indexRandom === indexRandom2){
+      var indexRandom2 = Math.floor(Math.random() * (lastIndexCards - firstIndexCards) + firstIndexCards);
+    }
+    var change = cards[indexRandom];
+    var change2 = cards [indexRandom2];
+    cards[indexRandom] = change2;
+    cards[indexRandom2] = change;
+  }
+  console.log ("Barajando...");
+}
+
+var Croupier = {baraja: barajar};
+
+Croupier.baraja(cards);
+
+
+/*function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}*/
